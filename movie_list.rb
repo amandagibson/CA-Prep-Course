c647826735e92c7f5b5e963c7888860d738a396d
amanda_gibson <gibson.amanda.mae@gmail.com>
2018-12-13 20:52:57 +0100

    movie list, names and years- puts years

---
+++ movie_list.rb
@@ -0,0 +1,6 @@
+movies = [{ name: 'American Beauty', year: 1999 }, { name: 'The Royal Tenenbaums', year: 2001 }, { name: 'Donnie Darko', year: 2001 }]
+
+movies.each do |movie|
+
+        puts "#{movie[:year]}"
+end
