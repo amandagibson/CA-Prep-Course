d0bca2a473ac7da7fa6222995bed77f243532582
amanda_gibson <gibson.amanda.mae@gmail.com>
2018-12-22 15:27:37 +0100

    changed capitalize mathod to it's own line

--- name.rb
+++ name.rb
@@ -1,11 +1,13 @@
 def greeting
 
     puts "What is your first name?"
-    first_name = gets.chomp.capitalize!
+    first_name = gets.chomp
+    first_name.capitalize!
 
     puts "What is your last name?"
-    last_name = gets.chomp.capitalize!
+    last_name = gets.chomp
+    last_name.capitalize!
 
-    puts "Hello, #{first_name } #{last_name}!"
+    puts "Hello, #{first_name } #{last_name}"
 
 end
