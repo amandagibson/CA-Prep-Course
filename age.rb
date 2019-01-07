00c8eb5bc01a58bff11b0e606b6430911b9ca349
amanda_gibson <gibson.amanda.mae@gmail.com>
2019-01-07 17:28:51 +0100

    attempt at fixing return of age

--- age.rb
+++ age.rb
@@ -7,6 +7,6 @@ years =[ a = 10, b = 20, c = 30, d = 40 ]
 
 years.each do |year|
 
-    puts "In #{year} years you will be ("#{current_age} + #{year}")."
+    puts "In #{year} years you will be " (return "#{current_age} + #{year}")."
 
 end
